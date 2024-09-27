d91713d4f944f0a9001305c10ea57224c5746697
hy <2864798982@qq.com>
2024-09-27 15:50:55 +0800

    链表相关力扣题目

---
+++ src/main/java/data_structure/linked_list/E08Leetcode876.java
@@ -0,0 +1,43 @@
+package data_structure.linked_list;
+
+/**
+ * 查找链表中间节点
+ */
+public class E08Leetcode876 {
+
+    /*
+                p1
+                        p2
+        1   2   3   4   5   null
+
+
+                    p1
+                                p2
+        1   2   3   4   5   6   null
+     */
+
+    /**
+     * 查找链表中间节点：快慢指针法解决
+     *
+     * @param head 头节点
+     * @return 中间节点
+     */
+    public ListNode middleNode(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+            p2 = p2.next;
+        }
+        return p1;
+    }
+
+    public static void main(String[] args) {
+        ListNode head1 = ListNode.of(1, 2, 3, 4, 5);
+        System.out.println(new E08Leetcode876().middleNode(head1));
+
+        ListNode head2 = ListNode.of(1, 2, 3, 4, 5, 6);
+        System.out.println(new E08Leetcode876().middleNode(head2));
+    }
+}
